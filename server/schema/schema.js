6f211c91d0f8e3cfd82758c86008de38f73a067c
Abrar Shariar <shariarabrar@gmail.com>
2019-05-25 20:05:17 +0600

    Nested data category add

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -13,24 +13,49 @@ const {
 } = require('../utils/http');
 
 const DEPT_URI = 'departments';
+const CATEGORY_URI = 'categories';
 
+// departments
 const DepartmentType = new GraphQLObjectType({
   name: 'Department',
   fields: () => ({
     department_id: { type: GraphQLInt },
     name: { type: GraphQLString },
+    description: { type: GraphQLString },
+    category: {
+      type: new GraphQLList(CategoryType),
+      resolve: async (parentValue, args) => {
+        return await get(`${CATEGORY_URI}/inDepartment/${parentValue.department_id}`);
+      }
+    }
+  })
+});
+
+
+// categories
+const CategoryType = new GraphQLObjectType({
+  name: 'Category',
+  fields: () => ({
+    category_id: { type: GraphQLInt },
+    name: { type: GraphQLString },
     description: { type: GraphQLString }
   })
 });
 
+
+// Root queries
 const RootQuery = new GraphQLObjectType({
   name: 'RootQuery',
   fields: {
-    department: {
+    dept: {
         type: new GraphQLList(DepartmentType),
-        args: { id: { type: GraphQLInt } },
+        args: { department_id: { type: GraphQLInt } },
         resolve: async (parentValue, args) => {
-          return args.id ? await get(`${DEPT_URI}/${args.id}`) : await get(`${DEPT_URI}`) ;
+          const data = args.department_id ? await get(`${DEPT_URI}/${args.department_id}`) : await get(`${DEPT_URI}`) ;
+          if (!Array.isArray(data)) {
+            return Array(data);
+          }
+          return data;
         }
     }
   }
